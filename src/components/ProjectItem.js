d3eb35b1fc76b1cbd6e4f3820bbed9c7579d0be9
BensonGitau <bensonkamaugitau@gmail.com>
2024-04-22 17:38:21 +0300

    complete lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,17 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
-  console.log(technologies)
   
-  const technologiesList = technologies.map((tech) => {
-    return <span key={tech}>{tech}</span>
-  })
+  
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-      <span>{technologiesList}</span>
+      <span>{technologies.map((tech) => {
+              return <span key={tech}>{tech}</span>
+          })
+        }
+      </span>
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,18 +2,13 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-
-  
-
-  const project = projects.map((project)=> {
-          return <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies}/>
-        })
-        console.log(project)
   return (
     <div id="projects">
       <h2>My Projects</h2>
       <div id="project-list">
-      {project}
+      {projects.map((project) => {
+        return <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies}/>
+      })}
       </div>
     </div>
   );
