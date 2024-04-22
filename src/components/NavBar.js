64ea929c475e48cdcce28cd198d0104534dceec1
BensonGitau <bensonkamaugitau@gmail.com>
2024-04-22 17:27:36 +0300

    Complete Lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const userLinks = links.map((link) =>{
+    return <a key={link} href={'#' + link}>{link}</a>
+  })
+  return <nav>{userLinks}</nav>;
 }
 
 export default NavBar;
